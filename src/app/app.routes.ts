cc03879352d53c282bd257e97a836c2c9d852f1e
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-05-01 11:23:22 +0530

    learned how to do lazy loading routing

---
+++ src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {path:"portfolio",loadChildren  : ()=>import('./portfolio/portfolio.module').then(m => m.PortfolioModule)},
+    ];
