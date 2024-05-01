3a7f6fc116b0640c91ef12a7383c1406d439310d
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-05-01 15:37:14 +0530

    Learned how to use ngFor and ngIf

--- src/app/portfolio/student/student.component.ts
+++ src/app/portfolio/student/student.component.ts
@@ -1,22 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-student',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './student.component.html',
   styleUrl: './student.component.scss'
 })
 export class StudentComponent {
-   name:string;
-    constructor(){
-      this.name = "john";
-    }    
-    greet(){
-      console.log("Helloo : "+this.name);
-    } 
+    userForm : any;
+    isFormVisible : boolean = true;
+    items : any[] = ['item 1',"item 2"," item 3"]
+   constructor(public formbuilder :FormBuilder){
+
+   }
+   ngOnInit(){
+    this.userForm = this.formbuilder.group({
+      name : ['',Validators.required],
+      email : [''],
+    })
+   };
+   onSubmit(){
+    console.log("Form Submit ",this.userForm.value)
+   }
 }
 
-let john = new StudentComponent();
+// let john = new StudentComponent();
 
-john.greet();
+// john.greet();
+
+//  name:string;
+  //   constructor(){
+  //     this.name = "john";
+  //   }    
+  //   greet(){
+  //     console.log("Helloo : "+this.name);
+  //   } 
