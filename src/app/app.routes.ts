1b022312a44d1e46ec22fa72287f553fc5ef9de4
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-05-03 17:51:15 +0530

    medical form module loaded

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,4 +2,5 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {path:"portfolio",loadChildren  : ()=>import('./portfolio/portfolio.module').then(m => m.PortfolioModule)},
-    ];
+    {path:"medical-form",loadChildren  : ()=>import('./medical-form/medical-form.module').then(m => m.MedicalFormModule)},
+];
