e65fd96a728a01cd24625f965dd34b1c16d2e0c6
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-05-04 16:47:54 +0530

    added reactive form validations

--- src/app/medical-form/evaluation/evaluation.component.ts
+++ src/app/medical-form/evaluation/evaluation.component.ts
@@ -1,30 +1,46 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { EmailValidator, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-evaluation',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule,NgSelectModule],
+  imports: [MatFormFieldModule, CommonModule, NgSelectModule, ReactiveFormsModule],
   templateUrl: './evaluation.component.html',
   styleUrl: './evaluation.component.scss'
 })
 export class EvaluationComponent {
-    medicalForm : any;
+  medicalForm: any;
+  constructor(private formBuilder:
+     FormBuilder) {
 
-    constructor(private formBuilder : FormBuilder ){
+  }
+
+  ngOnInit() {
+    this.initializeForm()
+  }
+
+  initializeForm() {
+    this.medicalForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.pattern(/^.{1,50}$/)]],
+      age: ['', [Validators.required, Validators.min(1), Validators.max(100)]],
+      gender: ['', Validators.required],
+      address: ['', Validators.required],
+      diagnosis: ['', Validators.required],
+      contact: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      email: ['', [Validators.required, Validators.email]],
+      personalHistory: [''],
+      familyHistory: [''],
+      painScore: ['']
+    });
+  }
+  submitForm() {
+    if (this.medicalForm.valid) {
+      console.log("Form is submitted", this.medicalForm.value);
+
     }
-    ngOnInit(){
-     this.intializeForm();     
-    }
-    intializeForm(){
-      this.medicalForm = this.formBuilder.group(
-        {
-          name : ['',[Validators.required]],
-          age : ['',[Validators.required,Validators.min(1),Validators.max(100)]],
-          email : ['',[Validators.required,Validators.email]]
-        }
-      )
-    }
+  }
+
 }
