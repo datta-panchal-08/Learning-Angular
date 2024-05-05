222b37c307bb75127ed3e001baf618eea9495531
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-05-05 09:43:24 +0530

    added error-handling component

--- src/app/portfolio/portfolio-routing.module.ts
+++ src/app/portfolio/portfolio-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StudentComponent } from './student/student.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { ErrorHandlingComponent } from './error-handling/error-handling.component';
 
 const routes: Routes = [
   {path :"student",component : StudentComponent},
-  {path: "about-us",component : AboutUsComponent}
+  {path: "about-us",component : AboutUsComponent},
+  {path : "error-handling", component : ErrorHandlingComponent}
 ];
 
 @NgModule({
